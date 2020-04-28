6ea656057bd5e8d46c0bba8f22b3e17f3ea99e24
DeedatBilla <deedat5@yahoo.com>
2020-04-28 08:31:57 +0000

    fix

--- app.js
+++ app.js
@@ -2,25 +2,24 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-
+const main = require("./src");
 const router = express.Router();
 const port = process.env.PORT;
-//const port =3000
+//const port = 3000;
 server.listen(port, () => console.log("server running on port:" + port));
 router.post("/sendMyLocation", async (req, res) => {
   const { longitude, latitude, riderEmail } = req.body;
+  res.send("socket started");
   io.on("connection", (socket) => {
-    socket.emit("clientlocation - " + riderEmail, { mylocation:req.body});
+    socket.emit("clientlocation - " + riderEmail, { mylocation: "hj" });
 
     socket.on("return my location", (data) => {
       console.log(data);
-      res.send({retun:data})
+      res.send(req.body);
     });
   });
 });
 app.use(express.json());
-app.use(router)
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`)
+app.use(router);
 
-// })
+
